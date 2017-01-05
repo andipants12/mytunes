05e05ccc12d8f2f0059900fe06661841dda8b23d
andipants12 <andreareneeo@gmail.com>
2017-01-04 17:43:38 -0800

    with this commit we finally figured out how to invoke playFirst when there is only one song in the queue!!!

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,7 +3,17 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
+  // length: this.set('length', this.length),
   initialize: function() {
+    // console.log(this.length, "initialize");
+    // this.playFirst();
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+  },
+  playFirst: function() {
+    console.log("meow?")
   }
-
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,8 +2,17 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.render();
+    this.collection.on('add', function() {
+      console.log('there was a change!');
+    })
   },
-
+  // events: {
+  //   'add': 'handleQueue'
+  // },
+  handleQueue: function() {
+    console.log("here!");
+  },
   render: function() {
     return this.$el;
   }
